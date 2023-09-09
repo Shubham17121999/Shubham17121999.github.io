79ed4ca1e3ab238e18a2ee7de15801aae7f6aca2
Shubham17121999 <shubhamkulkarni17121999@gmail.com>
2023-09-09 17:35:23 +0530

    Git updation

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+// Fetch GitHub calendar data using the GitHub API
+GitHubCalendar(".calendar", "Shubham17121999");
+
+// or enable responsive functionality:
+GitHubCalendar(".calendar", "Shubham17121999", { responsive: true });
+
+// Use a proxy
+GitHubCalendar(".calendar", "Shubham17121999", {
+   proxy (Shubham17121999) {
+     return fetch(`https://your-proxy.com/github?user=${Shubham17121999}`)
+   }
+}).then(r => r.text())
